95e7d839fe41466b273e85eaf645466c18025531
adityanichkawade <aditya.nichkawade@gmail.com>
2020-07-12 15:51:00 +0530

    Initial commit

---
+++ src/functional-patterns/curry.js
@@ -0,0 +1,6 @@
+const curry = (func) =>
+  function curried(...args) {
+    return func.length <= args.length
+      ? func.apply(this, args)
+      : (...args1) => curried.apply(this, args.concat(args1));
+  };
